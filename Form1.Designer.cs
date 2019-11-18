82a03ca75d71302ff1ed9b42c606d9482d7f3c0c
Zack-Correa <zackluthier@hotmail.com>
2019-11-18 14:44:24 -0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CircleFractals
+{
+    partial class CircleFractalsFr
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.minSizetxt = new System.Windows.Forms.TextBox();
+            this.radiustxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Location = new System.Drawing.Point(12, 88);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(900, 900);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(558, 6);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(162, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate \"inline\" fractal";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(558, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate fractal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // minSizetxt
+            // 
+            this.minSizetxt.Location = new System.Drawing.Point(328, 32);
+            this.minSizetxt.Name = "minSizetxt";
+            this.minSizetxt.Size = new System.Drawing.Size(100, 20);
+            this.minSizetxt.TabIndex = 3;
+            // 
+            // radiustxt
+            // 
+            this.radiustxt.Location = new System.Drawing.Point(154, 32);
+            this.radiustxt.Name = "radiustxt";
+            this.radiustxt.Size = new System.Drawing.Size(100, 20);
+            this.radiustxt.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Initial radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Min radius length";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(558, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(841, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CircleFractalsFr
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(928, 961);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radiustxt);
+            this.Controls.Add(this.minSizetxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CircleFractalsFr";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CircleFractals";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox minSizetxt;
+        private System.Windows.Forms.TextBox radiustxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CircleFractals
+{
+    public partial class CircleFractalsFr : Form
+    {
+        public CircleFractalsFr()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            mainPanel.Refresh();
+            DrawCircles(mainPanel.Height/2,mainPanel.Width/2 , radiustxt.Text == "" ? 400 : float.Parse(radiustxt.Text), minSizetxt.Text == ""? 20 : int.Parse(minSizetxt.Text), true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mainPanel.Refresh();
+            DrawCircles(mainPanel.Height / 2, mainPanel.Width / 2, radiustxt.Text == "" ? 400 : float.Parse(radiustxt.Text), minSizetxt.Text == "" ? 20 : int.Parse(minSizetxt.Text), false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mainPanel.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void DrawCircles(int x, int y, float radius, int minSize, bool inline)
+        {
+            Pen blackPen = new Pen(Color.Black, 0.05f);
+            Graphics panel = Graphics.FromHwnd(mainPanel.Handle);
+            Rectangle rect = new Rectangle(Convert.ToInt32(x - radius), Convert.ToInt32(y - radius), Convert.ToInt32(radius * 2), Convert.ToInt32(radius * 2));
+
+            panel.DrawEllipse(blackPen, rect);
+
+            if (radius > minSize)
+            {
+                DrawCircles(Convert.ToInt32(x + radius / 2), y, radius / 2, minSize, inline);
+                DrawCircles(Convert.ToInt32(x - radius / 2), y, radius / 2, minSize, inline);
+                if (!inline)
+                {
+                    DrawCircles(x, Convert.ToInt32(y - radius / 2), radius / 2, minSize, inline);
+                    DrawCircles(x, Convert.ToInt32(y + radius / 2), radius / 2, minSize, inline);
+                }
+            }
+
+        }
+    }
+}
